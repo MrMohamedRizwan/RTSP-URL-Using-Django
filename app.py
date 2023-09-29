c6f33cd350cabbe98d10fe52afcaae4ae1a73755
Mohamed Rizwan S <109430288+MrMohamedRizwan@users.noreply.github.com>
2023-09-29 22:58:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+RTSP_URL = "http://158.58.130.148/mjpg/video.mjpg"
+
+def generate_frames():
+    cap = cv2.VideoCapture(RTSP_URL)
+    
+
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if not ret:
+                break
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
